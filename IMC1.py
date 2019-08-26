c36191413d1250aede1b302d540646f2d2b4faf8
larissaplopes <larissacadastro@outlook.com>
2019-08-26 15:35:25 -0300

    apresentacao de mensagem de acordo com o valor de imc resultante

--- IMC1.py
+++ IMC1.py
@@ -5,7 +5,10 @@ imc = peso/altura**2
 if(imc < 17):
    print("Muito abaixo do peso ideal")
 elif(17 <= imc < 18.5):
-print("Abaixo do peso ideal", )
-print("Peso ideal", 18.5 <= imc < 25.0)
-print("Acima do peso ideal", 25.0 <= imc < 30)
-print("Muito acima do peso ideal", imc >= 30)
+   print("Abaixo do peso ideal")
+elif(18.5 <= imc < 25.0):
+   print("Peso ideal")
+elif(25.0 <= imc < 30):
+   print("Acima do peso ideal")
+else:
+   print("Muito acima do peso ideal", imc >= 30)
