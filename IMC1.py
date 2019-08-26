1f914252b512043555be78ef60b7fe9d94dad22c
larissaplopes <larissacadastro@outlook.com>
2019-08-26 15:33:34 -0300

    inclusao de entradas via input

--- IMC1.py
+++ IMC1.py
@@ -1,9 +1,11 @@
-peso = 70
-altura = 1.58
+peso = float(input("Informe seu peso: "))
+altura = float(input("Informe sua altura em metros: "))
 imc = peso/altura**2
 
-print("Muito abaixo do peso ideal", imc < 17)
-print("Abaixo do peso ideal", 17 <= imc < 18.5)
+if(imc < 17):
+   print("Muito abaixo do peso ideal")
+elif(17 <= imc < 18.5):
+print("Abaixo do peso ideal", )
 print("Peso ideal", 18.5 <= imc < 25.0)
 print("Acima do peso ideal", 25.0 <= imc < 30)
 print("Muito acima do peso ideal", imc >= 30)
