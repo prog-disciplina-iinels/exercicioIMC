77adc1609e0963b2f4ba2dde9d97fecd51272a43
Erika <erikagarcia512@gmail.com>
2019-08-26 14:20:58 -0300

    exercicio1-erika

--- erika_garcia/exercicio1.py
+++ erika_garcia/exercicio1.py
@@ -1,9 +1,9 @@
-peso = 70
-altura = 1.58
+peso = 56
+altura = 1.70
 imc = peso/altura**2
 
-print("Muito abaixo do peso ideal", imc < 17)
-print("Abaixo do peso ideal", 17 <= imc < 18.5)
-print("Peso ideal", 18.5 <= imc < 25.0)
-print("Acima do peso ideal", 25.0 <= imc < 30)
-print("Muito acima do peso ideal", imc >= 30)
+print("Muito abaixo do peso normal ", imc < 17)
+print("Abaixo do peso normal ", 17 <= imc < 18.5)
+print("Peso normal", 18.5 <= imc < 25.0)
+print("Acima do peso normal", 25.0 <= imc < 30)
+print("Muito acima do peso normal", imc >= 30)
